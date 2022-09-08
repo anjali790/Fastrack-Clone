5fa5c92b0b9ef977963d8a5e380842972dcc8b4c
anish0803 <105308559+anish0803@users.noreply.github.com>
2022-09-08 17:37:14 +0530

    product added

--- script.js
+++ script.js
@@ -32,3 +32,23 @@ document.querySelectorAll(".dot").forEach((bullet, bulletIndex) => {
     }
   });
 });
+
+
+// ------fastrack essential product start-----
+
+const productContainers = [...document.querySelectorAll('.product-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
