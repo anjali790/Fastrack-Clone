f384242cdf54741cde33fcbd2da94065f01c9662
anjali790 <anjaliruby790@gmail.com>
2022-09-12 18:27:14 +0530

    footer

--- Footer/app.js
+++ Footer/app.js
@@ -4,3 +4,11 @@ const lowerDiv = document.querySelector('.section3-lowerDiv');
 btn.addEventListener('click', function(){
     lowerDiv.classList.toggle("active");
 });
+
+
+// const trackOrder = document.querySelector('.track-order');
+// const section5 = document.querySelector('.section5');
+
+// trackOrder.addEventListener('click', function(){
+//     section5.style.display = 'block';
+// });
